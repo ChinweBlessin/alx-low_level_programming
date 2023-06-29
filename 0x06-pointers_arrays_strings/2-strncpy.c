dcfe7b5fe292a129a5c3a2adc82f3aa7ab3d5a65
ChinweBlessin <chinwesun2015@gmail.com>
2023-06-29 09:36:46 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,7 +16,7 @@ int count_of_bytes;
 for (count_of_bytes = 0; count_of_bytes < n && src[count_of_bytes] != '\0'; count_of_bytes++)
 dest[count_of_bytes] = src[count_of_bytes];
 
-for (; count_of_bytes = n; count_of_bytes++)
+for (; count_of_bytes < n; count_of_bytes++)
 dest[count_of_bytes] = '\0';
 
 return (dest);
