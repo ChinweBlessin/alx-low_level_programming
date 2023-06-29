a897a69137b773f7a1074802be86e3126eacc653
ChinweBlessin <chinwesun2015@gmail.com>
2023-06-29 09:30:58 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,6 +5,7 @@
  * @dest: destination string pointer.
  * @src: source string pointer.
  * @n: number of bytes to be used.
+ *
  * Return: pointer to destination string.
  */
 
@@ -13,6 +14,9 @@ char *_strncpy(char *dest, char *src, int n)
 int count_of_bytes;
 
 for (count_of_bytes = 0; count_of_bytes < n && src[count_of_bytes] != '\0'; count_of_bytes++)
+dest[count_of_bytes] = src[count_of_bytes];
+
+for (; count_of_bytes = n; count_of_bytes++)
 dest[count_of_bytes] = '\0';
 
 return (dest);
