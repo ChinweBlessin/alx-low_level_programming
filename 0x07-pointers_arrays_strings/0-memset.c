194e3bb62a8fdca984a5c7c4d544d022b3472778
ChinweBlessin <chinwesun2015@gmail.com>
2023-07-03 15:36:42 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@
 /**
  * _memset - fill a block of memory with a specific value
  * @s: starting address of memory to be filled
- * @b: th6e desired value
+ * @b: the desired value
  * @n: number of bytes to be changed
  *
  * Return: changed array with new value for n bytes
@@ -16,7 +16,7 @@ char *_memset(char *s, char b, unsigned int n)
 	for (; n > 0; i++)
 	{
 		s[i] = b;
-		n++;
+		n--;
 	}
 	return (s);
 }
